f44fa58ed706d7d9e9ca596485512ea43fa17fe1
Daan Rutten <daan@gfheu.org>
2018-06-12 12:02:27 +0200

    Initial commit

---
+++ Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+
+###########################
+#
+# Lua static library
+#
+###########################
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := lua
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/src
+
+LOCAL_SRC_FILES := \
+	$(subst $(LOCAL_PATH)/,, \
+	$(wildcard $(LOCAL_PATH)/src/*.c))
+
+include $(BUILD_STATIC_LIBRARY)
